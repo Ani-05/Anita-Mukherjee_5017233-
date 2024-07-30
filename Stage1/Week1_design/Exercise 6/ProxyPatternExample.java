f8e24cae39d0f105c73a215d295670b1643df24a
Anita Mukherjee <100989758+Ani-05@users.noreply.github.com>
2024-07-31 01:04:23 +0530

    ex 6

---
+++ Stage1/Week1_design/Exercise 6/ProxyPatternExample.java
@@ -0,0 +1,63 @@
+import java.util.Map;
+import java.util.HashMap;
+
+interface Image {
+    void display();
+}
+
+class RealImage implements Image {
+    private String fileName;
+
+    public RealImage(String fileName) {
+        this.fileName = fileName;
+        loadFromRemoteServer();
+    }
+
+    private void loadFromRemoteServer() {
+        System.out.println("Loading image from remote server: " + fileName);
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Displaying " + fileName);
+    }
+}
+
+class ProxyImage implements Image {
+    private RealImage realImage;
+    private String fileName;
+    private static Map<String, RealImage> imageCache = new HashMap<>();
+
+    public ProxyImage(String fileName) {
+        this.fileName = fileName;
+    }
+
+    @Override
+    public void display() {
+        if (imageCache.containsKey(fileName)) {
+            realImage = imageCache.get(fileName);
+        } else {
+            realImage = new RealImage(fileName);
+            imageCache.put(fileName, realImage);
+        }
+        realImage.display();
+    }
+}
+
+public class ProxyPatternExample {
+    public static void main(String[] args) {
+        Image image1 = new ProxyImage("img1.jpg");
+        Image image2 = new ProxyImage("img2.jpg");
+
+        image1.display();
+        System.out.println("");
+
+        image1.display();
+        System.out.println("");
+
+        image2.display();
+        System.out.println("");
+
+        image2.display();
+    }
+}
